a6b6f9d0b4833f844d5810ebfa8490fcc6bc037b
Ekin Kar <kar_ekin@outlook.com>
2024-06-16 12:54:03 +0300

    added header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="flex justify-between items-center max-w-6xl m-auto p-6">
+    <img :src="logoSrc" alt="Logo" class="w-36" />
+    <div class="flex space-x-4 gap-4">
+      <router-link to="/" class="hover:text-gray-500 cursor-pointer font-semibold"
+        >Home</router-link
+      >
+      <router-link to="/" class="hover:text-gray-500 cursor-pointer font-semibold"
+        >About</router-link
+      >
+      <router-link to="/" class="hover:text-gray-500 cursor-pointer font-semibold"
+        >Vehicles</router-link
+      >
+      <router-link to="/" class="hover:text-gray-500 cursor-pointer font-semibold"
+        >Our Team</router-link
+      >
+      <router-link to="/" class="hover:text-gray-500 cursor-pointer font-semibold"
+        >Contact</router-link
+      >
+    </div>
+    <div class="flex justify-center items-center gap-5">
+      <button class="font-semibold">Sign In</button>
+      <button class="font-semibold bg-custom-red py-3 px-6 text-white rounded-md">Sign Up</button>
+    </div>
+  </header>
+</template>
+
+<script>
+import logoSrc from '@/assets/logo.png'
+
+export default {
+  data() {
+    return {
+      logoSrc
+    }
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'custom-red': '#ff5230'
+      }
+    }
+  },
+  plugins: []
+}
