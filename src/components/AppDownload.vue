040cbcdcdfd97a94248c8da299f3af3756566db4
Ekin Kar <kar_ekin@outlook.com>
2024-06-17 20:25:45 +0300

    edited download section

--- src/components/AppDownload.vue
+++ src/components/AppDownload.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="h-screen">
-    <div class="h-96 w-full bg-slate-400 relative flex items-center">
-      <img
-        src="@/assets/background-image.png"
-        alt=""
-        class="absolute inset-0 w-full h-full object-cover"
-      />
-      <div class="absolute inset-0 flex items-center justify-start pl-32 bg-opacity-5 bg-black">
-        <div class="max-w-md mx-4 text-black">
-          <h1 class="text-4xl font-bold mb-2">Download our app!</h1>
-          <p class="text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis delectus consequuntur
-            nisi totam possimus sunt tempore, maiores at deserunt molestiae recusandae inventore
-            beatae maxime dolore? Hic doloribus exercitationem earum fuga.
-          </p>
-          <div class="flex gap-4 mt-2">
-            <img src="@/assets/play.png" alt="" />
-            <img src="@/assets/app.png" alt="" />
-          </div>
+  <div class="h-96 w-full bg-slate-400 relative flex items-center">
+    <img
+      src="@/assets/background-image.png"
+      alt=""
+      class="absolute inset-0 w-full h-full object-cover"
+    />
+    <div class="absolute inset-0 flex items-center justify-start pl-32 bg-opacity-5 bg-black">
+      <div class="max-w-md mx-4 text-black">
+        <h1 class="text-4xl font-bold mb-2">Download</h1>
+        <p class="text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis delectus consequuntur
+          nisi totam possimus sunt tempore, maiores at deserunt molestiae recusandae inventore
+          beatae maxime dolore? Hic doloribus exercitationem earum fuga.
+        </p>
+        <div class="flex gap-4 mt-2">
+          <img src="@/assets/play.png" alt="" />
+          <img src="@/assets/app.png" alt="" />
         </div>
       </div>
     </div>
