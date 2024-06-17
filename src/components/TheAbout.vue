8e607c67951ad81258c32b8f38b6d0d0dd2cb6ec
Ekin Kar <kar_ekin@outlook.com>
2024-06-17 13:11:08 +0300

    fixed about section

--- src/components/TheAbout.vue
+++ src/components/TheAbout.vue
@@ -1,27 +1,39 @@
 <template>
   <section class="py-20">
     <div class="container mx-auto text-center">
-      <h2 class="text-4xl font-bold">About Company</h2>
       <div class="mt-8 flex flex-col lg:flex-row items-center lg:justify-center lg:space-x-12">
         <img src="@/assets/about.jpg" alt="About Us" class="w-full max-w-sm mb-8 lg:mb-0" />
-        <div class="lg:text-left">
-          <p class="mt-4 text-gray-700 max-w-2xl">
+        <div class="lg:text-left flex flex-col justify-between gap-4">
+          <h2 class="text-4xl font-extrabold">About Company</h2>
+          <h3 class="text-3xl font-bold">You start the engine and your adventure begins</h3>
+          <p class="mt-4 text-gray-600 max-w-2xl">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nulla tenetur itaque
             velit nam perferendis doloremque nemo consequuntur dignissimos laboriosam culpa facilis
             quod aperiam esse, beatae cum, laudantium in repellendus.
           </p>
-          <div class="mt-8 flex flex-wrap justify-center space-x-12 lg:justify-start">
-            <div class="text-center lg:text-left">
-              <span class="block text-5xl font-bold text-red-500">20</span>
-              <span class="text-gray-700">Car Types</span>
+          <div
+            class="mt-8 flex flex-col lg:flex-row flex-wrap justify-center lg:justify-start items-center gap-6"
+          >
+            <div class="text-center lg:text-left flex flex-col gap-2">
+              <font-awesome-icon class="h-12 text-red-500" icon="fa-solid fa-car-side" />
+              <div class="flex justify-center items-center gap-2">
+                <span class="block text-4xl font-bold text-red-500">20</span>
+                <span class="text-gray-700 text-xl font-semibold">Car Types</span>
+              </div>
             </div>
-            <div class="text-center lg:text-left">
-              <span class="block text-5xl font-bold text-red-500">85</span>
-              <span class="text-gray-700">Rental Outlets</span>
+            <div class="text-center lg:text-left flex flex-col gap-2">
+              <font-awesome-icon class="h-12 text-red-500" icon="fa-solid fa-store" />
+              <div class="flex justify-center items-center gap-2">
+                <span class="block text-4xl font-bold text-red-500">85</span>
+                <span class="text-gray-700 text-xl font-semibold">Rental Outlets</span>
+              </div>
             </div>
-            <div class="text-center lg:text-left">
-              <span class="block text-5xl font-bold text-red-500">75</span>
-              <span class="text-gray-700">Repair Shops</span>
+            <div class="text-center lg:text-left flex flex-col gap-2">
+              <font-awesome-icon class="h-12 text-red-500" icon="fa-solid fa-screwdriver-wrench" />
+              <div class="flex justify-center items-center gap-2">
+                <span class="block text-4xl font-bold text-red-500">75</span>
+                <span class="text-gray-700 text-xl font-semibold">Repair Shops</span>
+              </div>
             </div>
           </div>
         </div>
--- src/main.js
+++ src/main.js
@@ -8,10 +8,22 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
   faCalendar,
   faCar,
+  faCarSide,
   faClock,
   faMapPin,
+  faScrewdriverWrench,
+  faStore,
   faUserSecret
 } from '@fortawesome/free-solid-svg-icons'
-library.add(faUserSecret, faClock, faCalendar, faMapPin, faCar)
+library.add(
+  faUserSecret,
+  faClock,
+  faCalendar,
+  faMapPin,
+  faCar,
+  faCarSide,
+  faStore,
+  faScrewdriverWrench
+)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
